26df6f50679f70fa1d29a6e9c474feae1201af2d
Aws12505 <awsaljaramani12505@gmail.com>
2025-11-01 05:27:51 -0400

    ..

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -5,7 +5,7 @@ export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
         <div className="flex aspect-circle size-8 items-center justify-center rounded-md  bg-gray-200 dark:bg-gray-800 text-sidebar-primary-foreground overflow-hidden">
                 <img 
                     src="/logo.png"
-                    alt="Trucking 360 Logo"
+                    alt="Swaida Academy Logo"
                     className="size-8 object-contain"
                 />
             </div>
--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -6,12 +6,12 @@ export default function AppLogo() {
             <div className="flex aspect-square size-8 items-center justify-center rounded-md  bg-gray-200 dark:bg-gray-800 text-sidebar-primary-foreground overflow-hidden">
                 <img 
                     src="/logo.png"
-                    alt="Trucking 360 Logo"
+                    alt="Swaida Academy Logo"
                     className="size-15 object-contain"
                 />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">Trucking 360 Website CMS</span>
+                <span className="mb-0.5 truncate leading-tight font-semibold">Swaida Academy CMS</span>
             </div>
         </>
     );
